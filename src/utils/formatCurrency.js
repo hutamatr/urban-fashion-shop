249ad72a0282a5a57031651b8c2c32a7a18b0ff6
hutamatr <hutamatr@gmail.com>
2022-08-07 02:52:01 +0700

    Add max-width for desktop and add background pattern

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -4,4 +4,13 @@ const formatCurrency = (amount) => {
   return toRupiah.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 };
 
+export const formatCurrencyOnly = (amount) => {
+  const exchangeRateRupiah = 14800;
+  return +amount * exchangeRateRupiah;
+};
+
+export const formatCurrencyToFixed = (amount) => {
+  return amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+};
+
 export default formatCurrency;
