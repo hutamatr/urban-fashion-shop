f59163e22fa7eee0454af1c6c533f942dd4b2d7f
hutamatr <hutamatr@gmail.com>
2022-08-06 19:20:12 +0700

    Create rating input & separate button elements into component

---
+++ src/components/UI/Rating.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import { MdOutlineStar } from "react-icons/md";
+
+const StarRating = ({ onRating }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  const mouseEnterHandler = (index) => setHover(index);
+  const mouseLeaveHandler = (rating) => setHover(rating);
+  const resetRatingHandler = () => {
+    setHover(0);
+    setRating(0);
+  };
+
+  const ratingHandler = (index) => {
+    setRating(index);
+    onRating(index);
+  };
+
+  return (
+    <div className="flex gap-x-1 py-2">
+      {[...Array(5)].map((_, index) => {
+        index += 1;
+        return (
+          <button
+            type="button"
+            key={index}
+            className={`bg-transparent duration-300 ${
+              index <= (rating && hover) ? "text-dark-brown" : "text-slate-100"
+            }`}
+            onClick={ratingHandler.bind(this, index)}
+            onDoubleClick={resetRatingHandler}
+            onMouseEnter={mouseEnterHandler.bind(this, index)}
+            onMouseLeave={mouseLeaveHandler.bind(this, rating)}
+          >
+            <span className="text-xl">
+              <MdOutlineStar />
+            </span>
+          </button>
+        );
+      })}
+      <span>{rating}/5</span>
+    </div>
+  );
+};
+
+export default StarRating;
