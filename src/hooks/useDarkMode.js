a3a65d76a1975854c841cac0508ca7148fdb6320
hutamatr <hutamatr@gmail.com>
2022-12-14 10:10:10 +0700

    create dark mode toggle

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const useDarkMode = () => {
+  const [theme, setTheme] = useState(localStorage.theme);
+  const colorTheme = theme === "dark" ? "light" : "dark";
+
+  useEffect(() => {
+    const rootElm = window.document.documentElement;
+    rootElm.classList.remove(colorTheme);
+    rootElm.classList.add(theme);
+
+    const bodyElm = window.document.body;
+    if (colorTheme === "light") {
+      bodyElm.style.backgroundColor = "#3F362F";
+    } else {
+      bodyElm.style.backgroundColor = "#E6E1DC";
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme, colorTheme]);
+
+  return { colorTheme, setTheme };
+};
+
+export default useDarkMode;
