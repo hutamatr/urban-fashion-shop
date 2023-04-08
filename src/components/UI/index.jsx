5c39763dd501228a6761b322664f10fbae5fc8d8
hutamatr <hutamatr@gmail.com>
2023-04-09 06:57:19 +0700

    remove unnecessary code

--- src/components/UI/index.jsx
+++ src/components/UI/index.jsx
@@ -7,4 +7,3 @@ export { Modal } from './Modal';
 export { Rating } from './Rating';
 export { ScrollTop } from './ScrollTop';
 export { SelectItems } from './SelectItems';
-export { Toast } from './Toast';
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import ContextProvider from './context/ContextProvider';
 
 import './index.css';
 
@@ -10,11 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ContextProvider>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
-      </ContextProvider>
+      <Routes>
+        <Route path='/*' element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
