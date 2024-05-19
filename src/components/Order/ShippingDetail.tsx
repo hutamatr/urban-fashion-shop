c188bb166ab9683d53cf5054a95e09422af31848
hutamatr <hutamatr@gmail.com>
2024-05-19 07:25:25 +0700

    feat: add real shipping data to shipping-detail from store

--- src/components/Order/ShippingDetail.tsx
+++ src/components/Order/ShippingDetail.tsx
@@ -1,13 +1,21 @@
 import OrderDetailItem from './OrderDetailItem';
 
-export default function ShippingDetail() {
+interface IShippingDetail {
+  shippingStatus: string;
+  shippingCost: string;
+  shippingAddress: string;
+}
+
+export default function ShippingDetail({
+  shippingStatus,
+  shippingCost,
+  shippingAddress,
+}: Readonly<IShippingDetail>) {
   return (
     <>
-      <OrderDetailItem title='Shipping Cost (Flat rate)' value='Rp. 15.000' />
-      <OrderDetailItem
-        title='Shipping Address'
-        value='Jalan Pahlawan No. 1 Jakarta Pusat Indonesia - 10110'
-      />
+      <OrderDetailItem title='Shipping Status' value={shippingStatus} />
+      <OrderDetailItem title='Shipping Cost (Flat rate)' value={shippingCost} />
+      <OrderDetailItem title='Shipping Address' value={shippingAddress} />
     </>
   );
 }
